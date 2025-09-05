355b3960ab97b9b72540cf6e0cc0a8d34213a0fe
Dipanshu Shukla <shuklascientist5@gmail.com>
2025-09-05 23:26:14 +0530

    refactor: dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM eclipse-temurin:21-jdk
+FROM eclipse-temurin:21-jdk-alpine-3.22
 ENV PORT=1337
 EXPOSE 1337
 COPY /target/url-shortener-shortening-service-0.0.1-SNAPSHOT.jar url-shortener-shortening-service.jar
--- src/main/java/com/shumisoft/url_shortener_shortening_service/repository/UrlMapRepository.java
+++ src/main/java/com/shumisoft/url_shortener_shortening_service/repository/UrlMapRepository.java
@@ -1,9 +1,11 @@
 package com.shumisoft.url_shortener_shortening_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.shumisoft.url_shortener_shortening_service.entity.UrlMap;
 
+@Repository
 public interface UrlMapRepository extends JpaRepository<UrlMap, Long> {
 
 }
